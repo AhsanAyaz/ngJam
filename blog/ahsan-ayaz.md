937b62c5446be4b1c9a9a8e3fb99bef45f6deff0
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-03-08 13:50:13 +0100

    feat(blog): added scully and initiated blog

---
+++ blog/ahsan-ayaz.md
@@ -0,0 +1,7 @@
+---
+title: ahsan ayaz
+description: blog description
+publish: false
+---
+
+# ahsan ayaz
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
+  {
+    path: '',
+    redirectTo: 'blog',
+    pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
